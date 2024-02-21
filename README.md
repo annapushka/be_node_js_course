ef830e96066a57d6df59b34f963fd254bded89b5
anna <anna.pushkareva@smddev.com>
2024-02-21 15:53:36 +0300

    add CRUD

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # be_node_js_course
+
+Backend on node.js express. RESTfull API, CRUD, HTTP
---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import env from './env.js';
+import router from './router.js';
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use('/api', router);
+
+async function startApp() {
+    try {
+        await mongoose.connect(env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+startApp();
